957092666c8fb3496f13b43b6f6ffb71334b0c37
Denis Vasilev <d89615022404@gmail.com>
2023-08-15 20:40:59 +0400

    начало урока 2.3

---
+++ hw-2.3.js
@@ -0,0 +1,10 @@
+let password = 'code123';
+
+let checkPassword = prompt('Введите пароль');
+
+if (password===checkPassword) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
+
